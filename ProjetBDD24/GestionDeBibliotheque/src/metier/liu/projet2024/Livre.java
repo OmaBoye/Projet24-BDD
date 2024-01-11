34223ac7f89338630e145f43c966b57880be9d2a
OmaBoye <154434702+OmaBoye@users.noreply.github.com>
2024-01-11 08:44:46 +0000

    Add files via upload

---
+++ ProjetBDD24/GestionDeBibliotheque/src/metier/liu/projet2024/Livre.java
@@ -0,0 +1,49 @@
+package metier.liu.projet2024;
+
+
+
+public class Livre {
+
+	private int ISBN ;
+	private String titre ;
+	private int IdAuteur ;
+	
+	//// constructeurs ////
+	public Livre(int iSBN, String titre, int idAuteur) {
+		super();
+		ISBN = iSBN;
+		this.titre = titre;
+		IdAuteur = idAuteur;
+	}
+	public Livre() {
+		
+	}
+	
+	/// getters et setters ///
+	public int getISBN() {
+		return ISBN;
+	}
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public int getIdAuteur() {
+		return IdAuteur;
+	}
+	public void setIdAuteur(int idAuteur) {
+		IdAuteur = idAuteur;
+	}
+	/// affichage///
+	@Override
+	public String toString() {
+		return "Livre [ISBN=" + ISBN + ", titre=" + titre + ", IdAuteur=" + IdAuteur + "]";
+	}
+	
+	
+	
+}
